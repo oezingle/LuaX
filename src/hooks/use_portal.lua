327088b7d5239f2e48a5a3cc7d65858b4d76b0ef
Zingle <oezingle@gmail.com>
2024-12-20 22:56:41 -0800

    Explicit error for use_portal when no portals available

--- src/hooks/use_portal.lua
+++ src/hooks/use_portal.lua
@@ -1,11 +1,11 @@
-
 local use_context = require("src.hooks.use_context")
 local Portal      = require("src.Portal")
 
 ---@alias LuaX.Hooks.UsePortal fun (name?: string): LuaX.Portal
 
-local function use_portal (name)
-    local portals = use_context(Portal.Context)
+local function use_portal(name)
+    local portals = assert(use_context(Portal.Context),
+        "No portals supplied! Are you sure a parent of this component is rendering its Provider?")
 
     local portal = portals[name or "LuaX.Portal"]
 
