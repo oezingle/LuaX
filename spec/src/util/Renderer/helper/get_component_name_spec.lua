0180c0dd3b884d66cc220142115db87703e244c7
Zingle <oezingle@gmail.com>
2025-01-15 17:07:36 -0800

    spec for get_component_name

---
+++ spec/src/util/Renderer/helper/get_component_name_spec.lua
@@ -0,0 +1,29 @@
+local get_component_name = require("src.util.Renderer.helper.get_component_name")
+local Inline = require("src.util.parser.Inline")
+
+describe("get_component_name", function()
+    it("gets the name of a classical component", function()
+        local Component = function()
+            return nil
+        end
+
+        assert.equal("Component", get_component_name(Component):match("^%S+"))
+    end)
+
+    it("gets the name of a transpiled component", function()
+        local Component = Inline:transpile_decorator(function()
+            return [[
+                <>
+
+                </>
+            ]]
+        end)
+
+        assert.equal("Component", get_component_name(Component):match("^%S+"))
+
+        collectgarbage("collect")
+
+        -- make sure our weak table is fine and dandy
+        assert.equal("Component", get_component_name(Component):match("^%S+"))
+    end)
+end)
