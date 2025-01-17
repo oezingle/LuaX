9080533f29a9aacc69af0d368f92c3f847c4c9fa
oezingle <oezingle@gmail.com>
2025-01-17 12:48:53 -0800

    Delete context directory

--- context/inherit.lua
+++
@@ -1,7 +0,0 @@
----@nospec
----@param caller LuaX.ElementNode?
-local function inherit_contexts(caller) if  not caller then return {} end
----@diagnostic disable-next-line:undefined-field
-local inherit=caller.props.__luax_internal.context
-return setmetatable({},{["__index"] = inherit}) end
-return inherit_contexts
