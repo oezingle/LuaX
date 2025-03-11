fd91625f13c9a1e717cc0f9cce37c975e5a42bf4
Zingle <oezingle@gmail.com>
2025-03-11 00:58:51 -0700

    Update path_dirname to perform better in edge cases

--- src/util/polyfill/path/dirname.lua
+++ src/util/polyfill/path/dirname.lua
@@ -2,10 +2,15 @@
 
 ---@param path string
 ---@return string
-local function path_basename(path)
-    path = path:gsub("[\\/][^\\/]+%.%S+$", "")
+local function path_dirname(path)
+    -- Path with no slashes, ie just a file
+    if not path:match("[/\\]") and path:match("%.") then
+        return ""
+    end
+ 
+    path = path:gsub("[/\\][^/\\]+%.%S+$", "")
 
     return path
 end
 
-return path_basename
+return path_dirname
